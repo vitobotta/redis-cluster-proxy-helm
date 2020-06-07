5b998ed24a6ad4756f300559cc74048e5201f0b4
Vito Botta <vito@botta.me>
2020-06-07 22:26:20 +0300

    Enable cross slots queries

--- docker/start-proxy.sh
+++ docker/start-proxy.sh
@@ -2,4 +2,4 @@
 
 sysctl -w net.core.somaxconn=65535
 
-/usr/local/bin/redis-cluster-proxy  --port $PORT --bind 0.0.0.0 $CLUSTER_ADDRESS
+/usr/local/bin/redis-cluster-proxy  --port $PORT --bind 0.0.0.0 --enable-cross-slot $CLUSTER_ADDRESS
